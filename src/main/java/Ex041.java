5909c0adb6b32f83c3e6f3fbbca65c11e1e6f8d1
劉鵬 <hou.ryuu920@gmail.com>
2022-11-11 13:49:56 +0900

    課題41

---
+++ src/main/java/Ex041.java
@@ -0,0 +1,40 @@
+
+/**
+ * プログラム名 :Ex041
+ * 引数 1 :なし
+ * 学籍番号 :22JY0140
+ * 名前 :劉　鵬
+ */
+public class Ex041 {
+
+    public static void main(String[] args) {
+        PlasticBottle pb = new PlasticBottle();
+        pb.size = 500;
+        pb.nokori = 500;
+        pb.show();
+        new PlasticBottle(100).show();
+        new PlasticBottle(350, 200).show();
+    }
+}
+
+class PlasticBottle {
+    int size;
+    int nokori;
+
+    PlasticBottle() {
+    }
+
+    PlasticBottle(int ml) {
+        size = ml;
+        nokori = 0;
+    }
+
+    PlasticBottle(int ml, int remain) {
+        size = ml;
+        nokori = remain;
+    }
+
+    void show() {
+        System.out.println("サイズは" + size + "ml、残りは" + nokori + "mlです");
+    }
+}
